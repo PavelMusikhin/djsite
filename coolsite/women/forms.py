417c428726e0d03b23a7babde6ed6b15e3260a5c
PavelMusikhin <muspasha@gmail.com>
2024-01-28 14:13:56 +0300

    first

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -7,18 +7,18 @@ from .models import *
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)  # инициировали класс ModelForm
-        self.fields['cat'].empty_label = "Категория не выбрана"
+        self.fields["cat"].empty_label = "Категория не выбрана"
 
     class Meta:  # используется для изменения поведения полей класса Модели
         model = Women
-        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
+        fields = ["title", "slug", "content", "photo", "is_published", "cat"]
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-input'}),
-            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+            "title": forms.TextInput(attrs={"class": "form-input"}),
+            "content": forms.Textarea(attrs={"cols": 60, "rows": 10}),
         }
 
     def clean_title(self):
-        title = self.cleaned_data['title']
+        title = self.cleaned_data["title"]
         if len(title) > 200:
-            raise ValidationError('Длина превышает 200 символов')
+            raise ValidationError("Длина превышает 200 символов")
         return title
